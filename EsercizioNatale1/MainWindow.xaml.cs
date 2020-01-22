d29a9c152f386620aa91d7fa8683258cf3a0ea20
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-22 13:23:27 +0100

    Add project files.

---
+++ EsercizioNatale1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EsercizioNatale1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_aggiungi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Visualizza_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> nomi = new List<string>();
+            List<double> voti = new List<double>();
+            StreamReader sr = new StreamReader("FileInvalido.txt");
+            string line;
+            while ((line = sr.ReadLine())!= null)
+            {
+                int index = line.IndexOf(",");
+                string nome = line.Substring(0, index);
+                string voto = line.Substring(index + 1);
+
+            }
+             
+
+
+
+        }
+    }
+}
